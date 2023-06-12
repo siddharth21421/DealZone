23947f4fa0083cf31ac70275996d14f8884eb4c0
unknown <siddharth.sasane@spit.ac.in>
2023-06-12 17:19:28 +0530

    new changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+       
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
